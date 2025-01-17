eecfbd21b66f40eab1a70d16417b92c4b8d7756f
NeunEinser <daniel30797@gmail.com>
2025-01-17 14:17:09 +0100

    Add optional delay argument for ping

--- src/discord_commands/ping.ts
+++ src/discord_commands/ping.ts
@@ -2,9 +2,25 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import { Command } from "../CommandRegistry";
 
 export default class PingCommand implements Command {
-	public static readonly data = new SlashCommandBuilder().setName('ping').setDescription('Checks if the bot is responsive');
+	public static readonly data = new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Checks if the bot is responsive')
+		.addIntegerOption(delay => delay
+			.setName("delay")
+			.setDescription("Delay in miliseconds")
+			.setMinValue(0)
+			.setMaxValue(600_000)
+			.setRequired(false)
+		);
 
 	async execute(interaction: ChatInputCommandInteraction) {
-		await interaction.reply("Pong!");
+		const delay = interaction.options.getInteger("delay");
+		if (delay) {
+			await interaction.deferReply();
+			await new Promise(resolve => setTimeout(resolve, delay));
+			await interaction.editReply("Pong!");
+		} else {
+			await interaction.reply("Pong!");
+		}
 	}
 }
